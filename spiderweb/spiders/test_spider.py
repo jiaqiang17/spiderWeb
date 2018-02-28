7d2437d15415b423e5856bc15e12d59cad646613
贾强-公司PC <jiaqiang17@126.com>
2018-02-28 16:10:37 +0800

    添加连接mongodb的函数
    连接上本地的mongodb来存储数据

--- spiderweb/spiders/test_spider.py
+++ spiderweb/spiders/test_spider.py
@@ -1,13 +1,29 @@
+#coding=utf-8
 import scrapy
+from pymongo import MongoClient
 
 class TestSpider(scrapy.spiders.Spider):
     name = "test"
-    allowed_domains= ["duobao.alibaba9.com/"]
+    allowed_domains= ["www.proginn.com/users/"]
     start_urls = [
-        "http://duobao.alibaba9.com/"
+        "https://www.proginn.com/users/"
     ]
 
     def parse(self, response):
-        filename = "testFile"
+
+        self.connectMongo()
+        filename = "testFile.html"
         with open(filename, "wb") as f:
             f.write(response.body)
+
+
+
+    def connectMongo(self):
+        # 建立MongoDB数据库连接
+        client = MongoClient('localhost', 27017)
+        # 连接所需数据库,test为数据库名
+        db = client.test
+        # 连接所用集合，也就是我们通常所说的表，test为表名
+        collection = db.test
+        # 查找集合中单条数据
+        print(collection.find_one())
